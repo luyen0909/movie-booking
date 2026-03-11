498332392add71618d765f6ac93ae3768046887d
luyen0909 <luyenluongpro0@gmail.com>
2026-03-11 18:10:05 +0700

    .

--- src/app/shared/components/movie-card/movie-card.ts
+++ src/app/shared/components/movie-card/movie-card.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.scss',
 })
-export class MovieCard {}
+export class MovieCard {
+  @Input() movie!: { title: string; image: string; rating: string; isHot?: boolean };
+}
