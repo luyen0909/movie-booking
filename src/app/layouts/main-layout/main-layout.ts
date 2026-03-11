5aec9baf39da1e9953f54033e56d03252b902027
luyen0909 <luyenluongpro0@gmail.com>
2026-03-11 17:43:50 +0700

    .

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from '../../shared/components/header/header';
+import { Navbar } from '../components/navbar/navbar';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, Header, Navbar, Footer], 
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
